65d9d69103dede4795bbeced780c735f0969013c
+ <+>
2022-02-14 22:32:38 -0500

    added audio and javascript

---
+++ index.js
@@ -0,0 +1,34 @@
+// cardinal-audio
+// blue-jay-audio
+// downy-audio
+// hawk-audio
+// hoodie-audio
+document.addEventListener("DOMContentLoaded", function(){
+    const birdCall = document.createElement("AUDIO");
+    const cardinal = document.getElementById("cardinal-audio");
+    cardinal.addEventListener("click", function() {
+        birdCall.setAttribute("src", "./assets/cardinals.wav");
+        birdCall.play();
+    });
+    const bluejay = document.getElementById("blue-jay-audio");
+    bluejay.addEventListener("click", function() {
+        birdCall.setAttribute("src", "./assets/blue-jays-trimmed.mp3");
+        birdCall.play();
+    });
+    const downy = document.getElementById("downy-audio");
+    downy.addEventListener("click", function() {
+        birdCall.setAttribute("src", "./assets/downy.flac");
+        birdCall.play();
+    });
+    const hawk = document.getElementById("hawk-audio");
+    hawk.addEventListener("click", function() {
+        birdCall.setAttribute("src", "./assets/red-tailed-hawk.wav");
+        birdCall.play();
+    });
+    const hoodie = document.getElementById("hoodie-audio");
+    hoodie.addEventListener("click", function() {
+        birdCall.setAttribute("src", "./assets/hooded-merganser.mp3");
+        birdCall.play();
+    });
+
+})
