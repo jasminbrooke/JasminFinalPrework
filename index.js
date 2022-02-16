f75178197aa3fc0feba066a27e5982f0e0927954
+ <+>
2022-02-16 00:44:31 -0500

    added on-hover sidebar with anchor links

--- index.js
+++ index.js
@@ -27,3 +27,22 @@ document.addEventListener("DOMContentLoaded", function(){
     });
 
 })
+
+var mini = true;
+
+function toggleSidebar() {
+  if (mini) {
+    console.log("opening sidebar");
+    document.getElementById("mySidebar").style.width = "250px";
+    this.mini = false;
+  }
+   else {
+    console.log("closing sidebar");
+    document.getElementById("mySidebar").style.width = "55px";
+    this.mini = true;
+  }
+}
+
+
+
+
